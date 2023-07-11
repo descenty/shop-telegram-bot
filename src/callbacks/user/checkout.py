0f9139592d18e11e6b80ed42af733101a692285a
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-07-11 21:09:00 +0300

    fix new order notifications

--- src/callbacks/user/checkout.py
+++ src/callbacks/user/checkout.py
@@ -27,81 +27,55 @@ async def execute(
     text = constants.language.unknown_error
 
     markup = [(constants.language.back, f'{{"r":"user","d":"cart"}}cancel')]
-    if checkout_settings["email"]:
-        text = constants.language.input_email
-        await states.Order.email.set()
-    elif checkout_settings["phone"]:
-        text = constants.language.input_phone
-        await states.Order.phone_number.set()
-    elif checkout_settings["address"]:
-        text = constants.language.input_address
-        await states.Order.address.set()
-    elif checkout_settings["captcha"]:
-        text = constants.language.input_captcha
-        markup = [
-            (constants.language.refresh, f'{{"r":"user"}}refresh')
-        ] + markup
-        await states.Order.captcha.set()
-    else:
-        # text = constants.language.input_comment
-        # await states.Order.comment.set()
+    order_items_amount = [
+        (models.items.Item(int(item_id)), amount)
+        for item_id, amount in (await user.cart.items.dict).items()
+    ]
+    cart_items_dict = [
+        {
+            "id": item.id,
+            "title": await item.name,
+            "amount": amount,
+            "price": await item.price,
+        }
+        for item, amount in order_items_amount
+    ]
+    cart_items = json.dumps(cart_items_dict)
 
-        # await callback_query.message.edit_text(
-        # text=text, reply_markup=markups.create(markup)
-        # )
+    await user.cart.items.clear()
 
-        # await states.Order.confirmation.set()
-        order_items_amount = [
-            (models.items.Item(int(item_id)), amount)
-            for item_id, amount in (await user.cart.items.dict).items()
-        ]
-        cart_items_dict = [
-            {
-                "id": item.id,
-                "title": await item.name,
-                "amount": amount,
-                "price": await item.price,
-            }
-            for item, amount in order_items_amount
-        ]
-        cart_items = json.dumps(cart_items_dict)
+    order = await models.orders.create(user_id=user.id, items_json=cart_items)
 
-        await user.cart.items.clear()
+    await callback_query.message.edit_text(
+        text=constants.language.confirm_order,
+        reply_markup=markups.create([]),
+    )
 
-        order = await models.orders.create(
-            user_id=user.id, items_json=cart_items
-        )
+    # notify all managers
 
-        await callback_query.message.edit_text(
-            text=constants.language.confirm_order,
-            reply_markup=markups.create([]),
-        )
+    managers = await models.users.get_managers()
 
-        # notify all managers
+    order_status = constants.OrderStatus(await order.status)
 
-        managers = await models.users.get_managers()
+    order_title_price = tuple(
+        (item.title, item.price) for item in await order.items
+    )
 
-        order_status = constants.OrderStatus(await order.status)
+    text = constants.language.new_order + "\n"
 
-        order_title_price = tuple(
-            (item.title, item.price) for item in await order.items
-        )
+    text += constants.language.format_manager_order(
+        order.id,
+        await order.date_created,
+        await user.username,
+        order_status_text[order_status],
+        order_title_price,
+        await order.total_price,
+    )
 
-        text = constants.language.new_order + "\n"
-
-        text += constants.language.format_order(
-            order.id,
-            await order.date_created,
-            await user.username,
-            order_status_text[order_status],
-            order_title_price,
-            await order.total_price,
-        )
-
-        for manager in managers:
-            try:
-                await constants.bot.send_message(manager.id, text)
-            except:
-                logging.error(
-                    f"Cannot send order notification to manager with id: {manager.id}"
-                )
+    for manager in managers:
+        try:
+            await constants.bot.send_message(manager.id, text)
+        except:
+            logging.error(
+                f"Cannot send order notification to manager with id: {manager.id}"
+            )
